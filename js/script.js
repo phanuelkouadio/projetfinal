bf1d7675f26a22ef3a34077cff12a89dac5be87c
phanuelkouadio <divinekouadio44@gmail.com>
2023-05-13 12:06:54 +0000

    test

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 // all begin here
+
+var modal = document.getElementById('id01');
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
